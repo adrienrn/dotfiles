b0452a1414f53a32a874f16744d1626d1f7c1062
Hussard <adrien.ricartnoblet@gmail.com>
2016-11-20 17:06:02 +0100

    Update install.sh for sublime/ (#8)

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 
 # Make symlink for a bunch of files.
 for file in $(find $DOTFILES_DIR -name bash_profile -o -name bashrc -o -name inputrc -o -name gitconfig -o -name emacs -type f); do
-    
+
     # Get the filename from path
     target=$(basename $file)
 
@@ -23,6 +23,22 @@ for file in $(find $DOTFILES_DIR -name bash_profile -o -name bashrc -o -name inp
     fi
 
     # Makes the symlink to dotfile.
-    ln -sfv $file ~/.$target
+    ln -sfv $file ~/.$target    
 done
 
+# Handle symlinks for Sublime Text 3
+SUBLIME_USERDIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+if [ $(uname) == "Darwin" ] && [ -d "$SUBLIME_USER_DIR" ]; then
+    for file in $(find "$DOTFILES_DIR/sublime"); do
+	# Get the filename from path
+	target=$(basename $file)
+
+	if [ -f ~/$target ] && [ ! -L ~/$target ]; then
+	    # Backup the existing file before linking.
+	    mv $SUBLIME_USERDIR/$target $SUBLIME_USERDIR/$target$date_suffix
+	fi
+
+	# Makes the symlink to dotfile.
+	ln -sfv $file $SUBLIME_USERDIR/$target
+    done
+fi
